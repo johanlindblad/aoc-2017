4a6bbff57cb696ddf13c2cf9de5f1afa379cc37b
Johan Lindblad <johanlindblad@gmail.com>
2017-12-07 19:09:43 +0100

    Slight cleanup

--- lib/aoc/day7/part2.ex
+++ lib/aoc/day7/part2.ex
@@ -17,21 +17,19 @@ defmodule Aoc.Day7.Part2 do
   3
 
   """
+  def mismatch(tree, _) when map_size(tree) == 0, do: 0
   def mismatch(tree, weights) do
-    subweights =
-      Map.keys(tree)
-      |> Enum.map(fn(key) -> weight(tree, weights, key) end)
+    subweights = Map.keys(tree)
+                 |> Enum.map(&(weight(tree, weights, &1)))
 
     if Enum.uniq(subweights) |> Enum.count > 1 do
-      odd_weight = unique_element(subweights)
-      odd_index = subweights |> Enum.find_index(&(&1 == odd_weight))
+      {_odd_weight, odd_index} = unique_element(subweights)
       odd_node = Map.keys(tree) |> Enum.at(odd_index)
       diff = Enum.max(subweights) - Enum.min(subweights)
 
-      if mismatch(tree[odd_node], weights) == 0 do
-        weights[odd_node] - diff
-      else
-        mismatch(tree[odd_node], weights)
+      case mismatch(tree[odd_node], weights) do
+        0 -> weights[odd_node] - diff
+        mismatch -> mismatch
       end
     else
       Map.values(tree)
@@ -41,22 +39,22 @@ defmodule Aoc.Day7.Part2 do
   end
 
   @doc """
-  Returns the one unique element
+  Returns the one unique element and its index
 
   ## Examples
 
   iex> Aoc.Day7.Part2.unique_element([2,1,1])
-  2
+  {2,0}
   iex> Aoc.Day7.Part2.unique_element([1,1,2])
-  2
+  {2,2}
 
   """
-  def unique_element([head | tail]), do: unique_element(tail, head)
-  def unique_element([head | tail], candidate) when head == candidate, do: unique_element(tail, candidate)
-  def unique_element([head], _candidate), do: head
-  def unique_element([head | tail], candidate), do: unique_element(tail, candidate, head)
-  def unique_element([head | _tail], candidate, other) when head == candidate, do: other
-  def unique_element([head | _tail], candidate, other) when head == other, do: candidate
+  def unique_element([head | tail]), do: unique_element(tail, {head, 0}, 0)
+  def unique_element([head | tail], {candidate, index}, at) when head == candidate, do: unique_element(tail, {candidate, index}, at + 1)
+  def unique_element([head], _candidate, at), do: {head, at + 1}
+  def unique_element([head | tail], {candidate, index}, at), do: unique_element(tail, {candidate, index}, {head, at}, at + 1)
+  def unique_element([head | _tail], {candidate, _}, {other, index}, _at) when head == candidate, do: {other, index}
+  def unique_element([head | _tail], {candidate, index}, {other, _}, _at) when head == other, do: {candidate, index}
 
   @doc """
   Calculates the weight of the tree
@@ -70,7 +68,7 @@ defmodule Aoc.Day7.Part2 do
   def weight(tree, weights, key) do
     subtree_weight = 
       Map.keys(tree[key])
-      |> Enum.map(fn(name) -> weight(tree[key], weights, name) end)
+      |> Enum.map(&(weight(tree[key], weights, &1)))
       |> Enum.sum
 
     weights[key] + subtree_weight
@@ -86,7 +84,6 @@ defmodule Aoc.Day7.Part2 do
 
   """
   def build_tree(descriptions), do: build_tree(Enum.into(descriptions, %{}), %{})
-  def build_tree(structure_map, tree) when map_size(structure_map) == 0, do: {structure_map, tree}
   def build_tree(structure_map, tree), do: build_tree(structure_map, tree, Part1.root_of(structure_map))
   def build_tree(structure_map, tree, node_name) do
     {children, structure_map} = Map.pop(structure_map, node_name)

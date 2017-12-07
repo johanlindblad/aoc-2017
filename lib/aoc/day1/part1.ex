76ff696592540cf68be0f242086f4530e62a5b26
Johan Lindblad <johanlindblad@gmail.com>
2017-12-07 21:40:36 +0100

    Refactor day 1 to a more fun, recursive thing

--- lib/aoc/day1/part1.ex
+++ lib/aoc/day1/part1.ex
@@ -1,17 +1,14 @@
 defmodule Aoc.Day1.Part1 do
-  def captcha(digits) do
-    length = String.length(digits)
-    split = 
-      String.split(digits, "")
-      |> Enum.reject(fn(c) -> String.length(c) == 0 end)
-      |> Enum.map(&String.to_integer/1)
-
-    Enum.reduce(0..(length-1), 0, fn(i, acc) ->
-      if Enum.at(split, i) == Enum.at(split, rem(i+1, length)) do
-        acc + Enum.at(split, i)
-      else
-        acc
-      end
-    end)
+  def captcha(digit_string) do
+    digits = String.graphemes(digit_string) |> Enum.map(&String.to_integer/1)
+    inner_captcha(digits)
   end
+
+  def inner_captcha(digits), do: inner_captcha(digits, [])
+  def inner_captcha([first | [second | rest]], remains) when first == second do
+    first + inner_captcha([second | rest], remains ++ [first])
+  end
+  def inner_captcha([first | [second | rest]], remains), do: inner_captcha([second | rest], remains ++ [first])
+  def inner_captcha([first], [other_first | _remains]) when first == other_first, do: first
+  def inner_captcha([_first], [_other_first | _remains]), do: 0
 end
--- lib/aoc/day1/part2.ex
+++ lib/aoc/day1/part2.ex
@@ -1,19 +1,13 @@
 defmodule Aoc.Day1.Part2 do
-  def captcha(digits) do
-    length = String.length(digits)
-    half_length = div(length, 2)
+  def captcha(digit_string) do
+    digits = String.graphemes(digit_string) |> Enum.map(&String.to_integer/1)
+    half_length = Enum.count(digits) |> div(2)
+    inner_captcha(digits, Enum.drop(digits, half_length))
+  end 
 
-    split = 
-      String.split(digits, "")
-      |> Enum.reject(fn(c) -> String.length(c) == 0 end)
-      |> Enum.map(&String.to_integer/1)
-
-    Enum.reduce(0..(length-1), 0, fn(i, acc) ->
-      if Enum.at(split, i) == Enum.at(split, rem(i + half_length, length)) do
-        acc + Enum.at(split, i)
-      else
-        acc
-      end
-    end)
+  def inner_captcha([], _), do: 0
+  def inner_captcha([first | rest], [other_first | other_rest]) when first == other_first do
+    first + inner_captcha(rest, other_rest ++ [first])
   end
+  def inner_captcha([first | rest], [other_first | other_rest]), do: inner_captcha(rest, other_rest ++ [first])
 end

bb0a27ba8b61a6e08a11b739bd1a96c13781d9bb
Johan Lindblad <johanlindblad@gmail.com>
2017-12-10 21:18:18 +0100

    Write a faster function for summing indices from lists
    Nice speedup for large n

--- lib/aoc/day3/part2.ex
+++ lib/aoc/day3/part2.ex
@@ -10,15 +10,17 @@ defmodule Aoc.Day3.Part2 do
   # Emits {[emitted elements], [accumulated previous squares]}
   def value_accumulator(_, []), do: {[1], [1]}
   def value_accumulator(offsets, previous_values) do
-    val = (Enum.map(offsets, &Enum.at(previous_values, -&1 - 1)) |> Enum.sum)
+    offsets = Enum.map(offsets, &(-&1 - 1))
+    val = sum_of(previous_values, offsets)
     {[val], [val | previous_values]}
   end
 
+  # Hard-code first three so that we are guaranteed unique indices
   def relative_indices(Part1.square(address: 1)), do: []
   def relative_indices(Part1.square(address: 2)), do: [-1]
+  def relative_indices(Part1.square(address: 3)), do: [-1, -2]
   def relative_indices(sq = Part1.square()) do
-    adjacent_offsets(sq) ++ inner_offsets(sq) ++ start_of_layer_offset(sq)
-    |> Enum.uniq
+    adjacent_offsets(sq) ++ start_of_layer_offset(sq) ++ inner_offsets(sq)
   end
 
   def adjacent_offsets(Part1.square(from_start: 0)), do: [-1]
@@ -29,10 +31,10 @@ defmodule Aoc.Day3.Part2 do
   def inner_offsets(sq = Part1.square(from_corner: 0)), do: [inner_offset(sq)]
   def inner_offsets(sq = Part1.square(layer: 2)), do: [inner_offset(sq)]
   def inner_offsets(sq = Part1.square(from_start: 0)), do: [inner_offset(sq)]
-  def inner_offsets(sq = Part1.square(from_start: 1)), do: [inner_offset(sq) - 1, inner_offset(sq)]
-  def inner_offsets(sq = Part1.square(to_corner: 1)), do: [inner_offset(sq) - 1, inner_offset(sq)]
-  def inner_offsets(sq = Part1.square(from_corner: 1)), do: [inner_offset(sq), inner_offset(sq) + 1]
-  def inner_offsets(sq), do: [inner_offset(sq) - 1, inner_offset(sq), inner_offset(sq) + 1]
+  def inner_offsets(sq = Part1.square(from_start: 1)), do: [inner_offset(sq), inner_offset(sq) - 1]
+  def inner_offsets(sq = Part1.square(to_corner: 1)), do: [inner_offset(sq), inner_offset(sq) - 1]
+  def inner_offsets(sq = Part1.square(from_corner: 1)), do: [inner_offset(sq) + 1, inner_offset(sq)]
+  def inner_offsets(sq), do: [inner_offset(sq) + 1, inner_offset(sq), inner_offset(sq) - 1]
 
   def start_of_layer_offset(Part1.square(to_next_layer: 1, from_start: index)), do: [-index]
   def start_of_layer_offset(Part1.square(to_next_layer: 2, from_start: index)), do: [-index]
@@ -48,4 +50,9 @@ defmodule Aoc.Day3.Part2 do
 
   def corners_passed(Part1.square(from_start: index, layer_width: width)), do: div(index, width)
   def inner_layer_width(Part1.square(layer_width: width)), do: width - 2
+
+  def sum_of(elements, indices, steps \\ 0)
+  def sum_of([head | tail], [steps | indices], steps), do: head + sum_of(tail, indices, steps + 1)
+  def sum_of(_, [], _), do: 0
+  def sum_of([_ | tail], indices, steps), do: sum_of(tail, indices, steps + 1)
 end

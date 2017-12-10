bdc1ceaf51a0cad025ea3398803969b44988dab1
Johan Lindblad <johanlindblad@gmail.com>
2017-12-10 22:27:49 +0100

    Use more record-based pattern matching on Day3

--- lib/aoc/day3/part1.ex
+++ lib/aoc/day3/part1.ex
@@ -13,15 +13,20 @@ defmodule Aoc.Day3.Part1 do
     Stream.iterate(square(), &next_square_after/1)
   end
 
-  def next_square_after({:square, 1, _, _, _, _, _, _}), do: {:square, 2, 2, 2, 0, 8, 1, 1}
-  def next_square_after({:square, address, layer, layer_width, _, 1, _, _}) do
-    to_next_layer = (layer_width * 4) + 8 + 1
-    {:square, address + 1, layer + 1, layer_width + 2, 0, to_next_layer - 1, 1, layer_width + 1}
+  def next_square_after(square(address: 1)), do: {:square, 2, 2, 2, 0, 8, 1, 1}
+  def next_square_after(sq = square(to_next_layer: 1, layer: layer, layer_width: layer_width)) do
+    increment(sq) |> square(
+      layer: layer + 1, layer_width: layer_width + 2,
+      from_start: 0, to_next_layer: (layer_width * 4) + 8,
+      from_corner: 1, to_corner: layer_width + 1
+    )
   end
-  def next_square_after({:square, address, layer, layer_width, from_start, to_next_layer, _, 1}) do
-    {:square, address + 1, layer, layer_width, from_start + 1, to_next_layer - 1, 0, layer_width}
+  def next_square_after(sq = square(to_corner: 1, layer_width: layer_width)) do
+    increment(sq) |> square(from_corner: 0, to_corner: layer_width)
   end
-  def next_square_after({:square, address, layer, layer_width, from_start, to_next_layer, from_corner, to_corner}) do
+  def next_square_after(sq = square()), do: increment(sq)
+
+  def increment({:square, address, layer, layer_width, from_start, to_next_layer, from_corner, to_corner}) do
     {:square, address + 1, layer, layer_width, from_start + 1, to_next_layer - 1, from_corner + 1, to_corner - 1}
   end
 
--- lib/aoc/day3/part2.ex
+++ lib/aoc/day3/part2.ex
@@ -1,6 +1,6 @@
 defmodule Aoc.Day3.Part2 do
   alias Aoc.Day3.Part1
-  require Aoc.Day3.Part1
+  import Aoc.Day3.Part1
   use Bitwise
 
   def relative_indices, do: Part1.squares |> Stream.map(&relative_indices/1)
@@ -16,40 +16,40 @@ defmodule Aoc.Day3.Part2 do
   end
 
   # Hard-code first three so that we are guaranteed unique indices
-  def relative_indices(Part1.square(address: 1)), do: []
-  def relative_indices(Part1.square(address: 2)), do: [-1]
-  def relative_indices(Part1.square(address: 3)), do: [-1, -2]
-  def relative_indices(sq = Part1.square()) do
+  def relative_indices(square(address: 1)), do: []
+  def relative_indices(square(address: 2)), do: [-1]
+  def relative_indices(square(address: 3)), do: [-1, -2]
+  def relative_indices(sq = square()) do
     adjacent_offsets(sq) ++ start_of_layer_offset(sq) ++ inner_offsets(sq)
   end
 
-  def adjacent_offsets(Part1.square(from_start: 0)), do: [-1]
-  def adjacent_offsets(Part1.square(from_corner: 1)), do: [-1, -2]
-  def adjacent_offsets(Part1.square(from_start: 1)), do: [-1, -2]
+  def adjacent_offsets(square(from_start: 0)), do: [-1]
+  def adjacent_offsets(square(from_corner: 1)), do: [-1, -2]
+  def adjacent_offsets(square(from_start: 1)), do: [-1, -2]
   def adjacent_offsets(_), do: [-1]
 
-  def inner_offsets(sq = Part1.square(from_corner: 0)), do: [inner_offset(sq)]
-  def inner_offsets(sq = Part1.square(layer: 2)), do: [inner_offset(sq)]
-  def inner_offsets(sq = Part1.square(from_start: 0)), do: [inner_offset(sq)]
-  def inner_offsets(sq = Part1.square(from_start: 1)), do: [inner_offset(sq), inner_offset(sq) - 1]
-  def inner_offsets(sq = Part1.square(to_corner: 1)), do: [inner_offset(sq), inner_offset(sq) - 1]
-  def inner_offsets(sq = Part1.square(from_corner: 1)), do: [inner_offset(sq) + 1, inner_offset(sq)]
+  def inner_offsets(sq = square(from_corner: 0)), do: [inner_offset(sq)]
+  def inner_offsets(sq = square(layer: 2)), do: [inner_offset(sq)]
+  def inner_offsets(sq = square(from_start: 0)), do: [inner_offset(sq)]
+  def inner_offsets(sq = square(from_start: 1)), do: [inner_offset(sq), inner_offset(sq) - 1]
+  def inner_offsets(sq = square(to_corner: 1)), do: [inner_offset(sq), inner_offset(sq) - 1]
+  def inner_offsets(sq = square(from_corner: 1)), do: [inner_offset(sq) + 1, inner_offset(sq)]
   def inner_offsets(sq), do: [inner_offset(sq) + 1, inner_offset(sq), inner_offset(sq) - 1]
 
-  def start_of_layer_offset(Part1.square(to_next_layer: 1, from_start: index)), do: [-index]
-  def start_of_layer_offset(Part1.square(to_next_layer: 2, from_start: index)), do: [-index]
+  def start_of_layer_offset(square(to_next_layer: 1, from_start: index)), do: [-index]
+  def start_of_layer_offset(square(to_next_layer: 2, from_start: index)), do: [-index]
   def start_of_layer_offset(_), do: []
 
-   def inner_offset(s = Part1.square()) do
-    -(4 * inner_layer_width(s)) - (2 * corners_passed(s)) - inner_offset_extra(s)
+   def inner_offset(sq = square()) do
+    -(4 * inner_layer_width(sq)) - (2 * corners_passed(sq)) - inner_offset_extra(sq)
   end
-  defp inner_offset_extra(Part1.square(from_corner: 0)), do: 2
-  defp inner_offset_extra(Part1.square(from_start: 0)), do: 0
-  defp inner_offset_extra(Part1.square(from_start: 1)), do: 0
+  defp inner_offset_extra(square(from_corner: 0)), do: 2
+  defp inner_offset_extra(square(from_start: 0)), do: 0
+  defp inner_offset_extra(square(from_start: 1)), do: 0
   defp inner_offset_extra(_), do: 1
 
-  def corners_passed(Part1.square(from_start: index, layer_width: width)), do: div(index, width)
-  def inner_layer_width(Part1.square(layer_width: width)), do: width - 2
+  def corners_passed(square(from_start: index, layer_width: width)), do: div(index, width)
+  def inner_layer_width(square(layer_width: width)), do: width - 2
 
   def sum_of(elements, indices, steps \\ 0)
   def sum_of([head | tail], [steps | indices], steps), do: head + sum_of(tail, indices, steps + 1)

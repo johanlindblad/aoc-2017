31bc6036ddaaeb21fb9c5d75526904a9fcb7678c
Johan Lindblad <johanlindblad@gmail.com>
2017-12-21 13:15:46 +0100

    Pre-cache all transformations into the patterns table in Day21

--- lib/aoc/day21/part1.ex
+++ lib/aoc/day21/part1.ex
@@ -4,6 +4,7 @@ defmodule Aoc.Day21.Part1 do
   def parse_transformations(input) do
     String.trim(input) |> String.split("\n") |> Enum.map(&parse_row/1)
     |> Enum.into(%{})
+    |> add_variations
   end
   def parse_row(row) do
     String.split(row, " => ") |> Enum.map(&parse_pattern/1) |> List.to_tuple
@@ -16,6 +17,7 @@ defmodule Aoc.Day21.Part1 do
   def num_squares(grid) do
     grid |> Enum.map(&Enum.sum/1) |> Enum.sum
   end
+
 
   def grid_stream({grid, width} \\ {@initial, 3}, patterns) do
     Stream.iterate({grid, width}, fn({grid, width}) ->
@@ -52,33 +54,13 @@ defmodule Aoc.Day21.Part1 do
   def iterate({grid, width}, patterns, chunk_size) do
     per_row = div(width, chunk_size)
     chunks = grid |> grid_to_chunks(chunk_size)
-    transformed = chunks |> Enum.flat_map(&(transform(&1, patterns)))
+    transformed = chunks |> Enum.flat_map(&(Map.fetch!(patterns, &1)))
 
     # Chunk the list of squares into chunks of 3 or 4 rows (which is the number
     # of rows in one transformation), take as many of these as there will be per
     # row of output and then transpose and flatten these to get the new output
     # grid
     transformed |> Enum.chunk_every(chunk_size+1) |> Enum.chunk_every(per_row) |> Enum.flat_map(&transpose/1) |> Enum.map(&List.flatten/1)
-  end
-
-  def transform(chunk, patterns) do
-    cond do
-      t = patterns[chunk] -> t
-      # Flip vertically
-      t = patterns[chunk |> Enum.reverse] -> t
-      # Flip horizontally
-      t = patterns[chunk |> Enum.map(&Enum.reverse/1)] -> t
-
-      # Rotate 90 deg to the right
-      t = patterns[chunk |> transpose] -> t
-      # Rotate 90 deg to the right and flip horizontally
-      t = patterns[chunk |> transpose |> Enum.reverse] -> t
-      # Rotate 90 deg to the right and flip vertically
-      t = patterns[chunk |> transpose |> Enum.map(&Enum.reverse/1)] -> t
-
-      # Rotate 90 deg to the left
-      t = patterns[chunk |> Enum.reverse |> transpose |> Enum.reverse] -> t
-    end
   end
 
   # Gives a list of chunks, which need to be mapped and then re-chunked to
@@ -88,6 +70,23 @@ defmodule Aoc.Day21.Part1 do
   end
   def chunk_row_chunk(row_chunk, chunk_width) do
     row_chunk |> Enum.map(&(Enum.chunk_every(&1, chunk_width))) |> transpose
+  end
+
+  def all_variations(pattern) do
+    Stream.cycle([:transpose, :rotate]) |> Enum.take(8)
+    |> Enum.reduce([pattern], fn
+      (:transpose, [head | tail]) -> [transpose(head), head | tail]
+      (:rotate, [head | tail]) -> [Enum.reverse(head), head | tail]
+    end)
+  end
+
+  def add_variations(patterns) do
+    Enum.reduce(Map.keys(patterns), patterns, fn(from, patterns) ->
+      to = patterns[from]
+      Enum.reduce(all_variations(from), patterns, fn(pattern, patterns) ->
+        Map.put(patterns, pattern, to)
+      end)
+    end)
   end
 
   def parse_pattern(coordinates \\ [[]], pattern, row \\ 0, column \\ 0)
